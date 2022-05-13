d8dfd74ca3139c4fc3f6c472973cd331100435f3
LiranWib <liran.barzilay@wib.com>
2022-05-13 18:40:51 +0300

    Created container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // name is not mendatory in the container but recommended
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        // auth: 'auto@http://localhost:8081/remoteEntry.js'
+      },
+      shared: packageJson.dependencies
+    }),
+  ]
+};
+
+module.exports = merge(commonConfig, prodConfig)
