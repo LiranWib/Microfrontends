2aa6e7de9f6bf2f204dc48210069ad9a71918c85
LiranWib <liran.barzilay@wib.com>
2022-05-15 14:51:28 +0300

    redeploying marketing

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,3 +1,4 @@
+
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
